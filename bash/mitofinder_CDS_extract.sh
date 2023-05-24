e8283b41cb3b6f3c21d0cfcd6fa6e1f739fac489
michaeltconnelly <mconnelly@rsmas.miami.edu>
2023-05-24 11:38:57 -0400

    add mitofinder CDS extract script

---
+++ bash/mitofinder_CDS_extract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# purpose: extract the CDS for the 13 protein-coding genes and 2 tRNAs annotated in Mitofinder results
+
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+
+# print message 
+for i in ${prodir}/outputs/mitofinder/*/*Final*/*contig.fasta ; do echo $(basename $i) ; grep -v ">" $i | wc -c ; done
+
+# extract CDS from the NT fasta files
+mtgenes="ATP8 COX1 rrnL ND1 CYTB ND2 ND6 ATP6 ND4 rrnS COX3 COX2 ND4L ND3 ND5"
+# prep directories and output files
+for gene in $mtgenes; do mkdir ${prodir}/outputs/mitofinder/cds_alignments/${gene} 
+touch ${prodir}/outputs/mitofinder/cds_alignments/${gene}/${gene}_all.fasta; done
+# nested for-loops to extract CDS from each sample into single file for each gene
+# complete assemblies
+for gene in $mtgenes; do for file in ${prodir}/outputs/mitofinder/final_results/*/*contig_genes_NT.fasta; do grep -A 1 ">*\@${gene}" $file >> ${prodir}/outputs/mitofinder/cds_alignments/${gene}/${gene}_all.fasta; done
+done
+# incomplete assemblies
+for gene in $mtgenes; do for file in ${prodir}/outputs/mitofinder/final_results/*/*final_genes_NT.fasta; do grep -A 1 ">*\@${gene}" $file >> ${prodir}/outputs/mitofinder/cds_alignments/${gene}/${gene}_all.fasta; done
+done
+
+# print message
+for gene in $mtgenes; do for i in ${prodir}/outputs/mitofinder/cds_alignments/${gene}/${gene}_all.fasta ; do lines=$(grep -v ">" $i | wc -l) ; echo "There are $lines seqs in $(basename $i)" ; done
+echo "MitoFinder CDS extraction and concatenation complete"
