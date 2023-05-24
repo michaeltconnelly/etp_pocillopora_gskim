19de762d6bdb46435f415c9f5d7549560774e927
michaeltconnelly <mconnelly@rsmas.miami.edu>
2023-05-24 11:46:49 -0400

    mitofinder extract CDS script worked!

--- bash/mitofinder_CDS_extract.sh
+++ bash/mitofinder_CDS_extract.sh
@@ -5,9 +5,11 @@ prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
 
 # extract CDS from the NT fasta files
 mtgenes="ATP8 COX1 rrnL ND1 CYTB ND2 ND6 ATP6 ND4 rrnS COX3 COX2 ND4L ND3 ND5"
-# prep directories and output files
+
+# prep directories and output files --> add flow control if statements
 for gene in $mtgenes; do mkdir ${prodir}/outputs/mitofinder/cds_alignments/${gene} 
 touch ${prodir}/outputs/mitofinder/cds_alignments/${gene}/${gene}_all.fasta; done
+
 # nested for-loops to extract CDS from each sample into single file for each gene
 # complete assemblies
 for gene in $mtgenes; do for file in ${prodir}/outputs/mitofinder/final_results/*/*contig_genes_NT.fasta; do grep -A 1 ">*\@${gene}" $file >> ${prodir}/outputs/mitofinder/cds_alignments/${gene}/${gene}_all.fasta; done
@@ -17,6 +19,6 @@ for gene in $mtgenes; do for file in ${prodir}/outputs/mitofinder/final_results/
 done
 
 # print message
+echo "MitoFinder CDS extraction and concatenation complete"
 for gene in $mtgenes; do for i in ${prodir}/outputs/mitofinder/cds_alignments/${gene}/${gene}_all.fasta ; do lines=$(grep -v ">" $i | wc -l) ; echo "There are $lines seqs in $(basename $i)" ; done ; done
 #
-echo "MitoFinder CDS extraction and concatenation complete"
