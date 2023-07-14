87d928586cf223b113882ada06f32fac3bc40535
michaeltconnelly <mconnelly@rsmas.miami.edu>
2023-07-14 09:01:53 -0400

    hydra scripts and jobs update 7-14

--- bash/spades_wrapper.sh
+++ bash/spades_wrapper.sh
@@ -2,21 +2,24 @@
 #./bash/spades_wrapper.sh
 #purpose: spades assembly
 
-#specify variable containing sequence file prefixes and directory paths
-mcs="/scratch/nmnh_corals/connellym"
-prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+# specify variable containing sequence file prefixes and directory paths to shorten paths 
+# scratch directory 
+mcs="/scratch/nmnh_corals/connellym" 
+# project root working directory
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim" 
 
 # making a list of sample names
 set=$1
 #files=$(ls /scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim/data/raw/)
 #samples=$(echo "$files" | cut -d . -f 1 | sort -u)
-samples=$(cat ${prodir}/data/${set}_samples.txt)
+samples=$(cat ${prodir}/data/${set}_samples.txt) # this can be any file that has a list of sample file prefixes
 
 #lets me know which files are being processed
-echo "These are the samples to be trimmed:"
+echo "These are the samples to be assembled:"
 echo $samples
 
-#loop to automate generation of scripts to direct sequence file trimming
+# loop to automate generation of scripts to direct sequence file trimming
+# edit output job script location within your project root directory
 for sample in $samples
 do \
 echo "Preparing script for ${sample}"
