6d62377bb522d15cc52ac20158742638287ae6c7
michaeltconnelly <mconnelly@rsmas.miami.edu>
2023-12-15 10:43:53 -0500

    align qc wrapper adjust for multiple genomes

--- bash/alignqc_wrapper.sh
+++ bash/alignqc_wrapper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# generate samtools flagstat reports for comparing read alignment rates to different Pocillopora reference genomes
 prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
-study="$1"
+set="$1"
+genome="$2"
 
 echo "# /bin/sh
 # ----------------Parameters---------------------- #
@@ -11,25 +13,25 @@ echo "# /bin/sh
 #$ -l mres=16G,h_data=1G,h_vmem=1G
 #$ -cwd
 #$ -j y
-#$ -N alignqc_${study}
-#$ -o ${prodir}/bash/jobs/alignqc_${study}.log
+#$ -N alignqc_${set}_${genome}
+#$ -o ${prodir}/bash/jobs/alignqc_${set}_${genome}.log
 #$ -m bea
-#$ -M connellym@si.edu" > $prodir/bash/jobs/alignqc_${study}.job
+#$ -M connellym@si.edu" > $prodir/bash/jobs/alignqc_${set}_${genome}.job
 #
-echo "#" >> $prodir/bash/jobs/alignqc_${study}.job
-echo "# ----------------Modules------------------------- #" >> $prodir/bash/jobs/alignqc_${study}.job
-echo "module load bioinformatics/samtools" >> $prodir/bash/jobs/alignqc_${study}.job
+echo "#" >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
+echo "# ----------------Modules------------------------- #" >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
+echo "module load bioinformatics/samtools" >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
 echo "#
 # ----------------Your Commands------------------- #
-#" >> $prodir/bash/jobs/alignqc_${study}.job
-echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME' >> $prodir/bash/jobs/alignqc_${study}.job
-echo 'echo + NSLOTS = $NSLOTS' >> $prodir/bash/jobs/alignqc_${study}.job
-echo "#" >> $prodir/bash/jobs/alignqc_${study}.job
-echo 'prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"' >> $prodir/bash/jobs/alignqc_${study}.job
+#" >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
+echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME' >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
+echo 'echo + NSLOTS = $NSLOTS' >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
+echo "#" >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
+echo 'prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"' >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
 # produce samtools flagstat reports for alignment QC
-echo 'for i in ${prodir}/outputs/alignments/*.sam; do name=$(basename $i | cut -d . -f 1); echo $name; samtools flagstat $i > ${prodir}/outputs/QCs/flagstats/${name}_flagstat.txt; done' >> $prodir/bash/jobs/alignqc_${study}.job
+echo 'for i in ${prodir}/outputs/alignments/${genome}/*.sam; do name=$(basename $i | cut -d . -f 1); echo $name; samtools flagstat $i > ${prodir}/outputs/QCs/flagstats/${genome}/${name}_${genome}_flagstat.txt; done' >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
 
-echo "echo = `date` job $JOB_NAME done" >> $prodir/bash/jobs/alignqc_${study}.job
+echo "echo = `date` job $JOB_NAME done" >> $prodir/bash/jobs/alignqc_${set}_${genome}.job
 
-qsub $prodir/bash/jobs/alignqc_${study}.job
+qsub $prodir/bash/jobs/alignqc_${set}_${genome}.job
 
