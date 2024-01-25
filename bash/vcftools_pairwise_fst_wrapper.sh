0a8860e6be40aaf3a9ee080acd97d056f641f093
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-01-25 16:22:12 -0500

    update paths

--- bash/vcftools_pairwise_fst_wrapper.sh
+++ bash/vcftools_pairwise_fst_wrapper.sh
@@ -8,7 +8,7 @@ prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
 VCF_NAME="$1"
 POPFILE="$2"
 
-touch ${prodir}/bash/jobs/vcftools_pairwise_fst.job
+touch ${prodir}/bash/jobs/vcftools_pairwise_fst_${POPFILE}.job
 
 echo "Preparing script for ${POPS}"
 #   input QSUB commands
@@ -29,7 +29,7 @@ module load bioinformatics/vcftools
 #
 # ----------------Your Commands------------------- #
 #
-#' > ${prodir}/bash/jobs/vcftools_pairwise_fst.job
+#' > ${prodir}/bash/jobs/vcftools_pairwise_fst_${POPFILE}.job
 #
 echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME
 #
@@ -37,19 +37,19 @@ VCF_NAME="$1"
 prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
 VCF_IN="${prodir}/outputs/${VCF_NAME}.vcf.gz"
 #
-#' >> ${prodir}/bash/jobs/vcftools_pairwise_fst.job
+#' >> ${prodir}/bash/jobs/vcftools_pairwise_fst_${POPFILE}.job
 #
 # generate list of populations
-POPS=$(cat ${prodir}/data/$POPFILE)
+POPS=$(cat ${prodir}/data/pops/$POPFILE)
 # calculate genome-wide per-site pairwise Fst
 # script to create commands for all pairwise comparisons
 set -- $POPS
 for a; do
     shift
     for b; do
-        printf 'vcftools --gzvcf $VCF_IN --weir-fst-pop ${prodir}/data/pops/%s --weir-fst-pop ${prodir}/data/pops/%s --out ${prodir}/outputs/%s_%s\n' "$a" "$b" "$a" "$b" >> ${prodir}/bash/jobs/vcftools_pairwise_fst.job
+        printf 'vcftools --gzvcf $VCF_IN --weir-fst-pop ${prodir}/data/pops/%s --weir-fst-pop ${prodir}/data/pops/%s --out ${prodir}/outputs/%s_%s\n' "$a" "$b" "$a" "$b" >> ${prodir}/bash/jobs/vcftools_pairwise_fst_${POPFILE}.job
     done
 done
 #
 
-echo 'echo = `date` job $JOB_NAME done' >> ${prodir}/bash/jobs/vcftools_pairwise_fst.job
+echo 'echo = `date` job $JOB_NAME done' >> ${prodir}/bash/jobs/vcftools_pairwise_fst_${POPFILE}.job
