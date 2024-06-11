7935768d07ba10e1a2c804c19546e823d7fb243f
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-06-11 14:22:42 -0400

    modify pairwise pcangsd wrapper

--- bash/pcangsd_pairwise_selection_wrapper.sh
+++ bash/pcangsd_pairwise_selection_wrapper.sh
@@ -3,9 +3,10 @@
 #purpose: perform PCA-based selection analyses between pairs of populations
 #input: lists of sample names
 
-#specify variable containing sequence file prefixes and directory paths
+# specify variable containing sequence file prefixes and directory paths
 prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
 angsddir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim/outputs/angsd"
+
 # list of populations
 POPFILE="$1"
 
@@ -50,15 +51,16 @@ angsddir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim/outputs/
 #' >> $JOBFILE
 
 # assign variable for pairwise comparison
-printf 'set="%s_%s"' "$pop1" "$pop2" >> $JOBFILE
+printf 'set="%s_%s"\n' "$pop1" "$pop2" >> $JOBFILE
 
+# input commands to create output directory and bam file list for each comparison
 echo 'samples=$(cat ${prodir}/data/${set}_samples.txt)
 
 # creating an output directory for set output files
-if [ ! -d "${prodir}/outputs/angsd/${set}" ]; then mkdir ${prodir}/outputs/angsd/${set}; fi
+if [ ! -d "${angsddir}/${set}" ]; then mkdir ${angsddir}/${set}; fi
 
 # assign variable for output directory
-setdir="${prodir}/outputs/angsd/${set}"
+setdir="${angsddir}/${set}"
 
 # making a list of bam file paths
 ls ${prodir}/outputs/alignments/*md.rg.bam | grep -f ${prodir}/data/${set}_samples.txt > ${setdir}/${set}_bamfile.txt
@@ -67,7 +69,7 @@ ls ${prodir}/outputs/alignments/*md.rg.bam | grep -f ${prodir}/data/${set}_sampl
 echo " These are the samples to be processed: ${samples}, there are $(cat ${setdir}/${set}_bamfile.txt | wc -l) in total"' >> $JOBFILE
 
 # input ANGSD commands
-echo '# prepare the BEAGLE input file' >> $JOBFILE
+echo '# prepare ANGSD variables' >> $JOBFILE
 
 # assign variable to sites and chrs files (sorted and indexed)
 echo 'SITES=${angsddir}/final_noclones/final_noclones_noLD_sorted_v3.sites.txt
@@ -83,6 +85,7 @@ echo 'FILTERS="-snp_pval 1e-5 -minMaf 0.05"' >> $JOBFILE
 echo 'TODO="-doMajorMinor 1 -doMaf 1 -doCounts 1 -doGeno 8 -doPost 1 -doGlf 2"' >> $JOBFILE
 
 # input ANGSD command to create BEAGLE file 
+echo '# create the BEAGLE input file' >> $JOBFILE
 echo 'angsd -sites $SITES -rf $CHRS -b $BAMS -GL 1 $FILTERS $TODO -P $NSLOTS -out ${setdir}/${set}_noLD_filtered' >> $JOBFILE
 
 # input PCangsd commands
