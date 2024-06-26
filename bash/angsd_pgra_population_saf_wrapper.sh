7440c0c69af80ea34ef2cffbf82c018633d7a33e
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-06-26 14:28:36 -0400

    add wrapper for SAF of pgrandis continental populations

---
+++ bash/angsd_pgra_population_saf_wrapper.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#./bash/angsd_population_saf_wrapper.sh
+#purpose: calculate population site allele frequency posteriors using ANGSD for a given list of populations
+#input: list of populations pointing to files with lists of sample names, aligned bam files
+
+#specify variable containing sequence file prefixes and directory paths
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+angsddir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim/outputs/angsd"
+# list of populations
+POPFILE="$1"
+
+# generate list of populations
+POPS=$(cat ${prodir}/data/$POPFILE)
+
+for set in $POPS; do 
+# make bam file lists
+ls ${prodir}/outputs/alignments/*md.rg.bam | grep -f ${prodir}/data/pops_pgrandis_continent/${set} > ${angsddir}/bamfiles/${set}_bamfile.txt
+# verify sample numbers are correct
+echo "For ${set}, There are $(cat ${angsddir}/bamfiles/${set}_bamfile.txt | wc -l) samples in total"
+# create job file
+echo "Creating job file for SAF estimation of ${set}"
+JOBFILE="${prodir}/bash/jobs/angsd_saf_${set}.job"
+touch $JOBFILE
+# input QSUB commands
+echo "#!/bin/sh
+# ----------------Parameters---------------------- #
+#$ -S /bin/sh
+#$ -pe mthread 8
+#$ -q mThM.q
+#$ -l mres=96G,h_data=12G,h_vmem=12G,himem
+#$ -cwd
+#$ -j y
+#$ -N angsd_saf_${set}
+#$ -o ${prodir}/bash/jobs/angsd_saf_${set}.log
+#$ -m bea
+#$ -M connellym@si.edu
+#
+# ----------------Modules------------------------- #
+module load bioinformatics/angsd
+#
+# ----------------Your Commands------------------- #
+#" > $JOBFILE
+# input job-specific variables
+echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME
+#
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+angsddir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim/outputs/angsd"
+set="$1"
+#' >> $JOBFILE
+# input ANGSD commands
+echo "${set} SAF estimation" >> $JOBFILE
+echo '# ----- SAF with ANGSD
+# specify variable with bam file list for each population / species
+BAMS="${angsddir}/bamfiles/${set}_bamfile.txt"
+# filters are not needed since we are restricting the SAF estimation to previously filtered sites (AllSites)
+# -doSaf 1: perform multisample GL estimation
+TODO=" -doSaf 1"
+# set ancestral reference genome fasta file
+#ANC="/scratch/nmnh_corals/connellym/sequences/pdam/pdam_genome.fasta"
+ANC="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim/data/seqs/peffusa_reference_ancestral.fa"' >> $JOBFILE
+echo '# estimate the site allele frequency likelihood
+angsd -sites ${angsddir}/AllSites.txt -rf ${angsddir}/SczhEnG.txt -b $BAMS -anc $ANC -GL 1 -P $NSLOTS $TODO -out ${angsddir}/${set} 
+echo "DONE!"' >> $JOBFILE
+# input job finished statment
+echo '#
+echo = `date` job $JOB_NAME done' >> $JOBFILE
+# submit job
+qsub $JOBFILE $set
+done
