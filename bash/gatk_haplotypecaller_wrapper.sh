d445d548bc5e3228023a32527add7a4cebd8e9ca
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-07-03 13:50:06 -0400

    fix java path with new module

--- bash/gatk_haplotypecaller_wrapper.sh
+++ bash/gatk_haplotypecaller_wrapper.sh
@@ -43,8 +43,8 @@ echo "#" >> ${prodir}/bash/jobs/${sample}_gatk_HC.job
 echo 'echo "This is the sample being processed:"' >> ${prodir}/bash/jobs/${sample}_gatk_HC.job
 echo "echo $sample" >> ${prodir}/bash/jobs/${sample}_gatk_HC.job
 #   input command for GATK
-echo "java -Xmx2g -jar /share/apps/bioinformatics/gatk/3.8.1.0/GenomeAnalysisTK.jar \
--T HaplotypeCaller \
+echo "java -Xmx2g -jar /share/apps/bioinformatics/gatk/4.5.0.0/gatk-package-4.5.0.0-local.jar HaplotypeCaller \
+HaplotypeCaller \
 -I ${prodir}/outputs/alignments/pgra_himb/${sample}.sorted.md.rg.bam \
 -o ${prodir}/outputs/alignments/gvcfs/${sample}.g.vcf.gz \
 -R ${mcs}/sequences/p_grandis_GCA_964027065.2/GCA_964027065.2_jaPocGran1.hap1.2_genomic.fna \
