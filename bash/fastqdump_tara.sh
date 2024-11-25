f9ed33be31e4973c58057886925afec45e79b4c5
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-11-25 15:44:30 -0500

    add fastqdump loop job

---
+++ bash/fastqdump_tara.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+study="Voolstra2023"
+samples=$(cat ${prodir}/data/tara_samples.txt)
+JOBFILE="$prodir/bash/jobs/fastqdump_${sample}.job"
+
+for sample in $samples
+do
+echo "# /bin/sh" > $JOBFILE
+#
+echo "# ----------------Parameters---------------------- #
+#$  -S /bin/sh
+#$ -pe mthread 8
+#$ -q mThM.q
+#$ -l mres=96G,h_data=12G,h_vmem=12G,himem
+#$ -cwd
+#$ -j y
+#$ -N fastqdump_${study}_${sample}
+#$ -o $prodir/bash/jobs/fastqdump_${study}_${sample}
+#$ -m bea
+#$ -M connellym@si.edu" >> $JOBFILE
+#
+echo "# ----------------Modules------------------------- #" >> $JOBFILE
+echo "module load bioinformatics/sratoolkit" >> $JOBFILE
+echo "#" >> $JOBFILE
+echo "# ----------------Your Commands------------------- #" >> $JOBFILE
+#
+echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME' >> $JOBFILE
+echo 'echo + NSLOTS = $NSLOTS' >> $JOBFILE
+#
+echo "fastq-dump --split-files --outdir ${prodir}/data/raw/ --gzip $sample" >> $JOBFILE
+#
+echo "#" >> $JOBFILE
+echo 'echo = `date` job $JOB_NAME done' >> $JOBFILE
+qsub $JOBFILE
+done
