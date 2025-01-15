6be2beca5312cfe49837b9836c5fd3d417ac154e
michaeltconnelly <mconnelly@rsmas.miami.edu>
2025-01-15 09:57:40 -0500

    add psbA blast wrapper

---
+++ bash/noncoral_spades_blastn_psbA_wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# symbiont barcode: psbA
+# C. latusorum, C. pacificum, D.glynni
+
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+
+for infile in $(ls ${prodir}/outputs/noncoral_spades/contigs/*_contigs.fasta)
+do
+     # Use previously made BLAST databases from non-coral assemblies 
+     base=$(basename ${infile} _contigs.fasta)
+     # psbA genes
+     # C. latusorum
+     blastn -query ${prodir}/data/seqs/C_lat_psbA_MW819757.fasta -db ${prodir}/outputs/noncoral_spades/blastdbs/${base} -outfmt 6 -perc_identity 100 -evalue 1e-50 -out ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_C_lat_psbA
+     sort -k3,3g -k12,12gr -k11,11g  ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_C_lat_psbA | sort -u -k1,1 --merge > ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_C_lat_psbA.sorted
+     # C. pacificum 
+     blastn -query ${prodir}/data/seqs/C_pac_psbA_MW861711.fasta -db ${prodir}/outputs/noncoral_spades/blastdbs/${base} -outfmt 6 -perc_identity 100 -evalue 1e-50 -out ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_C_pac_psbA
+     sort -k3,3g -k12,12gr -k11,11g  ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_C_pac_psbA | sort -u -k1,1 --merge > ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_C_pac_psbA.sorted
+     # D. glynni
+     blastn -query ${prodir}/data/seqs/D_gly_psbA_KY114947.fasta -db ${prodir}/outputs/noncoral_spades/blastdbs/${base} -outfmt 6 -perc_identity 100 -evalue 1e-50 -out ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_D_gly_psbA
+     sort -k3,3g -k12,12gr -k11,11g  ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_D_gly_psbA | sort -u -k1,1 --merge > ${prodir}/outputs/noncoral_spades/psbA_blast_hits/${base}_D_gly_psbA.sorted
+done
+
+
+# Put top hits from all samples into one text file
+# python blast_sum.py
