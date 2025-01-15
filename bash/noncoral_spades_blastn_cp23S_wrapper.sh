8bacb2f6851a49240b528f1e31d164046260f604
michaeltconnelly <mconnelly@rsmas.miami.edu>
2025-01-15 09:53:29 -0500

    add cp23S blast search wrapper

---
+++ bash/noncoral_spades_blastn_cp23S_wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# symbiont barcode: cp23S
+# C. latusorum, C. pacificum, D.glynni
+
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+
+for infile in $(ls ${prodir}/outputs/noncoral_spades/contigs/*_contigs.fasta)
+do
+     # Use previously made BLAST databases from non-coral assemblies 
+     base=$(basename ${infile} _contigs.fasta)
+     # cp23S genes
+     # C. latusorum
+     blastn -query ${prodir}/data/seqs/C_lat_cp23S_MW711735.fasta -db ${prodir}/outputs/noncoral_spades/blastdbs/${base} -outfmt 6 -perc_identity 100 -evalue 1e-50 -out ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_C_lat_cp23S
+     sort -k3,3g -k12,12gr -k11,11g  ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_C_lat_cp23S | sort -u -k1,1 --merge > ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_C_lat_cp23S.sorted
+     # C. pacificum 
+     blastn -query ${prodir}/data/seqs/C_pac_cp23S_MW711736.fasta -db ${prodir}/outputs/noncoral_spades/blastdbs/${base} -outfmt 6 -perc_identity 100 -evalue 1e-50 -out ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_C_pac_cp23S
+     sort -k3,3g -k12,12gr -k11,11g  ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_C_pac_cp23S | sort -u -k1,1 --merge > ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_C_pac_cp23S.sorted
+     # D. glynni
+     blastn -query ${prodir}/data/seqs/D_gly_cp23S_KY114948.fasta -db ${prodir}/outputs/noncoral_spades/blastdbs/${base} -outfmt 6 -perc_identity 100 -evalue 1e-50 -out ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_D_gly_cp23S
+     sort -k3,3g -k12,12gr -k11,11g  ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_D_gly_cp23S | sort -u -k1,1 --merge > ${prodir}/outputs/noncoral_spades/cp23S_blast_hits/${base}_D_gly_cp23S.sorted
+done
+
+
+# Put top hits from all samples into one text file
+# python blast_sum.py
