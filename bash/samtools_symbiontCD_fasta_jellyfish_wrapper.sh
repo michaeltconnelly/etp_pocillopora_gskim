7a1f5e1df7a548c03e9b1ea7a336cadca328c7a1
michaeltconnelly <mconnelly@rsmas.miami.edu>
2025-01-22 11:35:29 -0500

    add job for samtools fasta CD generation, jellyfish kmer counting

---
+++ bash/samtools_symbiontCD_fasta_jellyfish_wrapper.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#./bash/samtools_symbiontCD_fasta_jellyfish_wrapper.sh
+#purpose: convert sorted, indexed symbiont_alignments to fasta read files with samtools, count kmers with jellyfish
+
+#specify variable containing sequence file prefixes and directory paths
+mcs="/scratch/nmnh_corals/connellym"
+prodir="/scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim"
+
+# making a list of sample names
+set=$1
+#files=$(ls /scratch/nmnh_corals/connellym/projects/etp_pocillopora_gskim/data/raw/)
+#samples=$(echo "$files" | cut -d . -f 1 | sort -u)
+samples=$(cat ${prodir}/data/${set}_samples.txt)
+
+#lets me know which files are being processed
+echo "These are the samples to be processed:"
+echo $samples
+
+#loop to automate generation of scripts to direct sequence file trimming
+for sample in $samples
+do \
+JOBFILE="${prodir}/bash/jobs/${sample}_symCD_samtools_split.job"
+touch $JOBFILE
+#
+echo "Preparing script for ${sample}"
+#   input QSUB commands
+echo "# /bin/sh" > $JOBFILE
+echo "# ----------------Parameters---------------------- #" >> $JOBFILE
+echo "#$  -S /bin/sh
+#$ -pe mthread 8
+#$ -q mThM.q
+#$ -l mres=96G,h_data=12G,h_vmem=12G,himem" >> $JOBFILE
+echo "#$ -j y
+#$ -N ${sample}_symCD_samtools_jellyfish
+#$ -o ${prodir}/bash/jobs/${sample}_symCD_samtools_jellyfish.log
+#$ -m bea
+#$ -M connellym@si.edu" >> $JOBFILE
+#
+echo "# ----------------Modules------------------------- #" >> $JOBFILE
+echo "module load bioinformatics/samtools" >> $JOBFILE
+echo "start-conda" >> $JOBFILE
+echo "conda activate d2ssect" >> $JOBFILE
+#
+echo "# ----------------Your Commands------------------- #" >> $JOBFILE
+#
+echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME' >> $JOBFILE
+echo 'echo + NSLOTS = $NSLOTS' >> $JOBFILE
+
+echo 'echo "Starting samtools fasta generation"' >> $JOBFILE
+
+#   input command to produce fasta files for both Cladocopium and Durusdinium alignments
+echo "samtools fasta ${prodir}/outputs/symbiont_alignments/${sample}_symCD.sorted.bam \
+-o ${prodir}/outputs/symbiont_kmers/${sample}_symCD.fasta -@ 8"  >> $JOBFILE
+
+#   input command for jellyfish kmer counting
+echo "jellyfish count -m 21 -s 10000000 ${prodir}/outputs/symbiont_kmers/${sample}_symCD.fasta \
+-o ${prodir}/outputs/symbiont_kmers/${sample}_symCD.jf -t 8" >> $JOBFILE
+
+echo 'echo '${sample}' successfully processed' >> $JOBFILE
+#
+echo 'echo = `date` job $JOB_NAME done' >> $JOBFILE
+# submit job
+qsub $JOBFILE
+#
+sleep 0.2
+done
